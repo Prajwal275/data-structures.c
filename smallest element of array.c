fa911c1c3370f0b57d1d9f5589699c661c2b4c0e
Prajwal275 <tprajwal905@gmail.com>
2025-02-11 13:51:44 +0530

    smallest element of array.c

---
+++ smallest element of array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int arr[5]={10,4,7,12,11};
+    int min=arr[0];
+    for (int i=0;i<=4;i++){
+        if(min>arr[i]){
+        }
+    }
+    printf("smallest element is %d",min);
+}
